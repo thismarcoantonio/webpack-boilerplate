061c23b9bb7bd7edb3fd0cab904e0725b699fd99
thismarcoantonio <mjota51@gmail.com>
2018-05-27 22:56:28 -0300

    [upd] adding babel, webpack and npm configuration;

---
+++ client/webpack.config.babel.js
@@ -0,0 +1,23 @@
+import path from 'path'
+
+export default {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, use: ['style-loader', 'css-loader', 'sass-loader'], exclude: /node_modules/ }
+    ]
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: process.env.PORT || 3000
+  }
+  // plugins: [
+  //   new HtmlWebpackPlugin({ template: './src/index.'}/)
+  // ]
+}
